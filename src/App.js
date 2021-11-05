6514586220f4f24061fb0ea08564b7b023c49d12
Cecilia Liliana Riveros Chirino <cchirino@nubenc.com>
2021-11-05 12:29:22 -0300

    subiendo proyecto en react

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+        Bienvenido a Emotions, mi nombre es Cecilia Riveros.
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
