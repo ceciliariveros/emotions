50018d13304e1f2e745a2e95f5331bbd41d20129
ceciliariveros <ceciliariveros0210@gmail.com>
2021-12-08 13:54:29 -0300

    se completo desafio 8

--- src/components/itemCount/ItemCount.js
+++ src/components/itemCount/ItemCount.js
@@ -1,33 +1,32 @@
 import React, {useState} from 'react'
 import './ItemCount.css'
 
-export default function ItemCount() {
-    const maxStock=10;
-    const [counter, setCounter] = useState(0)
-    const [stock, setStock] =useState(10)
-    const agregarAlCarrito = ()=>{
-        if(counter<maxStock){
-            setCounter(counter+1)
-            setStock(maxStock-(counter+1))
-        }
+export default function ItemCount({onAdd}) {
+    // const maxStock=10;
+    // const [counter, setCounter] = useState(0)
+    // const [stock, setStock] =useState(10)
+    // const agregarAlCarrito = ()=>{
+    //     if(counter<maxStock){
+    //         setCounter(counter+1)
+    //         setStock(maxStock-(counter+1))
+    //     }
 
-    }
-    const quitarDelCarrito = ()=>{
-        if(counter>0){
-            setCounter(counter-1)
-            setStock(maxStock-(counter-1))
-        }
-    }
+    // }
+    // const quitarDelCarrito = ()=>{
+    //     if(counter>0){
+    //         setCounter(counter-1)
+    //         setStock(maxStock-(counter-1))
+    //     }
+    // }
     return (
         <div className="contenedorCouter" >
 
-            <h3 className="title">Remera Roja</h3>
-            <p className="textP">Cantidad en el Carrito: {counter}</p>
+            {/* <p className="textP">Cantidad en el Carrito: {counter}</p> */}
             <div className="groupButton">
-                <button className="buttonAgregar" onClick={agregarAlCarrito} >Agregar al Carrito</button>
-                <button className="buttonQuitar" onClick={quitarDelCarrito}> Quitar del Carrito</button>
+                <button className="buttonAgregar" onClick={()=>onAdd(1)} >Agregar al Carrito</button>
+                <button className="buttonQuitar" onClick={()=>onAdd(-1)}> Quitar del Carrito</button>
             </div>
-            <p className="textStock">Stock Disponible: {stock}</p>
+            {/* <p className="textStock">Stock Disponible: {stock}</p> */}
 
            
         </div>
--- src/components/itemDetail/ItemDetail.js
+++ src/components/itemDetail/ItemDetail.js
@@ -1,7 +1,25 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './ItemDetail.css'
+import ItemCount from '../itemCount/ItemCount'
+import {NavLink} from 'react-router-dom'
+
 
 const ItemDetail = ({item}) => {
+
+    const maxStock=item.available_quantity;
+    const [counter, setCounter] = useState(0)
+    const [stock, setStock] =useState(maxStock)
+    
+    function onAdd (cantidad){
+        if((counter<=maxStock)&& (counter>=0)){
+            if(!((counter==0) && (cantidad<0))){
+                setCounter(counter+cantidad)
+                setStock(maxStock-(counter+cantidad))
+            }
+        }
+
+    }
+    
     return (
         <div className="contenedor">
             <div className="cardItem">
@@ -9,6 +27,20 @@ const ItemDetail = ({item}) => {
                 <h4>$ {item.price}</h4>
                 <h3>{item.title}</h3>
             </div>
+            <ItemCount onAdd={onAdd}></ItemCount>
+            <p className="cantidadCarro">Stock disponible: {stock}</p>
+            <p className="cantidadCarro">Cantidad del carrito: {counter}</p>
+
+            {
+                counter>0 ?
+                    <button className="buttonCompra">
+                        <NavLink to="/cart"  >Terminar compra</NavLink>
+                    </button>
+                    :
+                    null
+            }
+
+           
         </div>
     )
 }
