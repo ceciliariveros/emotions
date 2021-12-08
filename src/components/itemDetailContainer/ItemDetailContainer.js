f4d7f84cc4af93e57487c3589a04f0363461f9b7
ceciliariveros <ceciliariveros0210@gmail.com>
2021-12-08 12:00:25 -0300

    se arreglo error de que cargaba siempre el mismo producto

--- src/components/itemDetailContainer/ItemDetailContainer.js
+++ src/components/itemDetailContainer/ItemDetailContainer.js
@@ -2,18 +2,22 @@ import React, {useState, useEffect} from 'react'
 import ItemDetail from '../itemDetail/ItemDetail'
 import { Loader } from 'semantic-ui-react'
 import './ItemDetailContainer.css'
+import { useParams } from 'react-router'
 
 
 const ItemDetailContainer = () => {
    const [item, setItem] = useState([])
    const [loading,setLoading] = useState(true)
+   let paramsId = useParams();
+   let id = paramsId.id;
 
    useEffect(() => {
      setTimeout(()=>{
-         fetch('https://api.mercadolibre.com/sites/MLA/search?category=MLA1574&limit=1')
+         fetch(`https://api.mercadolibre.com/items/${id}`)
          .then((response)=>response.json())
          .then((respJSON)=>{
-             setItem(respJSON.results[0]);
+             console.log('respuesta', respJSON)
+             setItem(respJSON);
              setLoading(false);
             })
          .catch((error)=>console.log('Error inesperado: ',error))
